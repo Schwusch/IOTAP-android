a89ba54981e844f88530435920afb5965569ece9
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2016-11-15 09:34:16 +0100

    New program structure

--- app/src/main/java/com/example/schwusch/iotap_app/MainActivity.java
+++ app/src/main/java/com/example/schwusch/iotap_app/MainActivity.java
@@ -24,19 +24,25 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
-    BluetoothAdapter mBluetoothAdapter;
-    ConnectThread mConnectThread;
-    BluetoothDevice mDevice;
-    ConnectedThread ct;
     TextView tvText;
-    StringBuilder log = new StringBuilder();
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
+
 
     protected void logToTextView(final String text) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                log.append(text + "\n");
-                tvText.setText(log.toString());
+                // UI stuff here in case of calling from another thread
+                tvText.setText(text);
             }
         });
     }
@@ -53,40 +59,52 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                String msg = "*";
-                if (ct != null) ct.write(msg.getBytes());
+                try
+                {
+                    findBT();
+                    openBT();
+                }
+                catch (Exception ex) {
+                    tvText.setText("An error occured. Check debug logs.");
+                    ex.printStackTrace();
+                }
             }
         });
+    }
 
+    void findBT() {
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        // Check if bluetooth is enabled
-        if (!mBluetoothAdapter.isEnabled()) {
-            // Ask to enable
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            logToTextView("Asking for bluetooth?");
-            startActivityForResult(enableBtIntent, 1);
-        } else {
-            logToTextView("Bluetooth enabled :D");
+        if (mBluetoothAdapter == null) {
+            tvText.setText("No bluetooth adapter available");
         }
 
-        // Find the "wristband" and connect to it
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         if (pairedDevices.size() > 0) {
             for (BluetoothDevice device : pairedDevices) {
                 if (device.getName().equals("IOTAP")) {
-                    logToTextView("Found IOTAP paired device in list.");
-                    mDevice = device;
-                    mConnectThread = new ConnectThread(mDevice);
-                    mConnectThread.start();
+                    mmDevice = device;
+                    break;
                 }
             }
         }
+        tvText.setText("Bluetooth Device Found");
+    }
 
-        if (mConnectThread == null) {
-            logToTextView("No arduino was found...");
-        }
+    void openBT() throws IOException {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
+        mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        mmSocket.connect();
+        mmOutputStream = mmSocket.getOutputStream();
+        mmInputStream = mmSocket.getInputStream();
 
+        beginListenForData();
+
+        tvText.setText("Bluetooth Opened");
     }
 
     @Override
@@ -111,119 +129,60 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//-------------------------- PRIVATE CLASSES--------------------------
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        final byte delimiter = 10; //This is the ASCII code for a newline character
 
-    private class ConnectThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final BluetoothDevice mmDevice;
-        private final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
 
-        // Create a bluetooth socket
-        public ConnectThread(BluetoothDevice device) {
-            BluetoothSocket tmp = null;
-            mmDevice = device;
-
-            try {
-                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
-            } catch (IOException e) {
-            }
-
-            mmSocket = tmp;
-        }
-
-        // Connect to socket and start sending
-        public void run() {
-            mBluetoothAdapter.cancelDiscovery();
-            try {
-                logToTextView("Connecting to Arduino...");
-                mmSocket.connect();
-            } catch (IOException connectException) {
-                logToTextView("Connecting failed!");
-                try {
-                    mmSocket.close();
-                } catch (IOException closeException) {
-                }
-                return;
-            }
-            ct = new ConnectedThread(mmSocket);
-            ct.start();
-        }
-
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) {
-            }
-        }
-    }
-
-    private class ConnectedThread extends Thread {
-        final byte delimiter = 10;
-        private final BluetoothSocket mmSocket;
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-
-        int readBufferPosition = 0;
-        byte[] readBuffer = new byte[1024];
-
-        public ConnectedThread(BluetoothSocket socket) {
-            Looper.prepare();
-            mmSocket = socket;
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-            try {
-                tmpIn = socket.getInputStream();
-                tmpOut = socket.getOutputStream();
-            } catch (IOException e) {
-            }
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-            logToTextView("Connected to Arduino");
-        }
-
-        public void run() {
-            boolean stopWorker = false;
-            while(!Thread.currentThread().isInterrupted() && !stopWorker) {
-                try {
-                    int bytesAvailable = mmInStream.available();
-                    if (bytesAvailable > 0) {
-                        byte[] packetBytes = new byte[bytesAvailable];
-                        mmInStream.read(packetBytes);
-                        for (int i = 0; i < bytesAvailable; i++) {
-                            byte b = packetBytes[i];
-                            if (b == delimiter) {
-                                byte[] encodedBytes = new byte[readBufferPosition];
-                                System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
-                                final String data = new String(encodedBytes, "US-ASCII");
-                                readBufferPosition = 0;
-
-                                logToTextView("Arduino says:" + data);
-                            } else {
-                                readBuffer[readBufferPosition++] = b;
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                                            logToTextView(data);
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
                             }
                         }
                     }
-                } catch (IOException ex) {
-                    stopWorker = true;
-                    logToTextView("IOException when receiving data!");
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
                 }
             }
-        }
+        });
 
-        public void write(byte[] bytes) {
-            try {
-                mmOutStream.write(bytes);
-                Log.d("DEBUG", "Message sent");
-            } catch
-                    (IOException e) {
-            }
-        }
-
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) {
-            }
-        }
+        workerThread.start();
     }
 }
