bd0010a09d90103dd1f8b69ca512740b149a5c29
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2016-11-16 09:21:34 +0100

    Removed code

--- app/src/main/java/com/example/schwusch/iotap_app/MainActivity.java
+++ app/src/main/java/com/example/schwusch/iotap_app/MainActivity.java
@@ -12,8 +12,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -107,28 +105,6 @@ public class MainActivity extends Activity {
         tvText.setText("Bluetooth Opened");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     void beginListenForData()
     {
         mServiceIntent = new Intent(this, DataCollectorService.class);
@@ -138,7 +114,7 @@ public class MainActivity extends Activity {
     // Broadcast receiver for receiving status updates from the DataCollectorService
     private class ResponseReceiver extends BroadcastReceiver
     {
-        // Prevents instantiation
+        // Prevents instantiation?
         private ResponseReceiver() {
         }
         // Called when the BroadcastReceiver gets an Intent it's registered to receive
