ac3ec25ac088dc990729c3fbbee55a6b41171e33
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2016-12-13 19:07:17 +0100

    Now compatible with API 23

--- app/src/main/java/com/example/schwusch/iotap_app/GestureDetector.java
+++ app/src/main/java/com/example/schwusch/iotap_app/GestureDetector.java
@@ -1,27 +1,19 @@
 package com.example.schwusch.iotap_app;
 
 import android.app.Activity;
-import android.content.Context;
 import android.util.Log;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.ArrayList;
-import java.util.Queue;
 
 import weka.classifiers.Classifier;
-import weka.core.SerializationHelper;
 
 class GestureDetector {
     private ArrayList<LinkedList<Integer>> movingWindow = new ArrayList<>();
     private ArrayList<LinkedList<Integer>> filteredMovingWindow = new ArrayList<>();
     private Classifier cls;
-    Activity mainActivity;
+    private Activity mainActivity;
 
     GestureDetector(Activity mainActivity) throws Exception {
         initMovingWindows();
@@ -34,8 +26,10 @@ class GestureDetector {
         if (values != null) {
             // If window is full, dequeue one sample from head before queuing another
             if (movingWindow.get(0).size() > Constants.MOVING_WINDOW_SIZE - 1) {
-                movingWindow.forEach(Queue::poll);
-                filteredMovingWindow.forEach(Queue::poll);
+                for (int i = 0; i < movingWindow.size(); i++) {
+                    movingWindow.get(i).poll();
+                    filteredMovingWindow.get(i).poll();
+                }
             }
 
             // Save sample to moving window tail
@@ -54,16 +48,22 @@ class GestureDetector {
             Log.d("SAMPLE_ERROR", "Number of values:" + parts.length);
             return null;
         } else {
-            return (Integer[]) Arrays.stream(Arrays.copyOfRange(parts, 1, parts.length)).
-                    map(Integer::parseInt).toArray();
+            Integer values[] = new Integer[Constants.SENSOR_VALUES];
+            for (int i = 1; i < parts.length; i++) {
+                values[i] = Integer.parseInt(parts[i]);
+            }
+            return values;
         }
     }
 
     private Integer filterFIR(LinkedList<Integer> data) {
         LinkedList<Integer> lastValues = (LinkedList<Integer>)
                 data.subList(data.size() - Constants.MOVING_AVERAGE_LENGTH, data.size());
-
-        return lastValues.stream().reduce(0, (a, b) -> a + b) / lastValues.size();
+        Integer sum = 0;
+        for (Integer val: lastValues) {
+            sum += val;
+        }
+        return sum / lastValues.size();
     }
 
     private void initMovingWindows() {
